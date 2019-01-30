41b006af532584f4cafa139e8b15a3567a3204b2
David Henry <mgadriver@gmail.com>
2019-01-30 12:41:56 +0200

    getchildren works for all combinations no family, with family no children, with family with children

--- GedcomToSQL/src/tree/treeTest.java
+++ GedcomToSQL/src/tree/treeTest.java
@@ -61,9 +61,12 @@ public class treeTest {
 		}
 
 		Node<String> root = new Node<>("root");
-		int [] children = getdbChildren(1);
-		for (int c: children)
-			root.addChild(new Node<String>("node " + c));
+		int [] children = getChildren(1,1);  // 1 family, 2 kids
+//		children = getChildren(879,1);  // no family
+//	    children = getChildren(30,1);  // 1 family, no kids
+//      children = getChildren(55,1);  // 3 families, 1,0,3 kids
+//		for (int c: children)
+//			root.addChild(new Node<String>("node " + c));
 		//System.out.println(children);
 		//Node<String> root = createTree();
 		printTree(root, "-");
@@ -90,39 +93,65 @@ public class treeTest {
 		node.getChildren().forEach(each ->  printTree(each, appender + appender));
 	}
 	
-	private static int [] getdbChildren(int start) {
-		int [] children;
-		int family;
-		String myfamily = "";
-		String myChildren = "";
-		ResultSet rs;
-		String sql = "Select ownfamily from Individual where id="+start+";";
- 		try {
- 			rs = stmt.executeQuery(sql);
- 			while (rs.next())
- 		    {
- 				myfamily = rs.getString("OwnFamily");
- 		    }
- 		    stmt.close();
- 		} catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
- 		family = Integer.parseInt(myfamily);
-		sql = "Select children from Family where id="+family+";";
- 		try {
- 			rs = stmt.executeQuery(sql);
- 			while (rs.next())
- 		    {
- 				myChildren = rs.getString("children");
- 		    }
- 		    stmt.close();
- 		} catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
- 		String [] allChildren = myChildren.split(",");
- 		children = new int[allChildren.length];
- 		for (int i=0; i< allChildren.length;i++)
- 			children[i] = Integer.parseInt(allChildren[i]);
-		return children;
+	private static int[] getChildren(int parent, int depth)
+	{
+		int [] children = null;
+		List totalChildren = new ArrayList();
+		ResultSet ownfamilyset,childrenset;
+		String myfamilies = null;
+		String myChildren = null;
+		String sql = "Select ownfamily from Individual where id="+parent+";";
+		// check if parent has any families
+		try {
+			Statement Fstmt = conn.createStatement();
+			ownfamilyset = Fstmt.executeQuery(sql);
+			// NOTE java.sql.resultset cannot tell you how many rows there are
+			while (ownfamilyset.next())
+			{
+				myfamilies = ownfamilyset.getString("OwnFamily");
+				// myfamilies is a CSV of family(families)
+				if (myfamilies != null)
+				{
+					String [] Families = myfamilies.split(",");
+					// loop over all families
+					for (int c=0; c<Families.length;c++)
+					{
+						sql = "Select children from Family where id="+Families[c]+";";
+						try {
+							Statement Cstmt = conn.createStatement();
+							childrenset = Cstmt.executeQuery(sql);
+							while (childrenset.next())
+							{
+								myChildren = childrenset.getString("children");
+								// myChildren is a CSV of children
+								if (myChildren != null)
+								{
+									String [] allChildren = myChildren.split(",");
+									for (int i=0; i< allChildren.length;i++)
+									{
+										totalChildren.add(Integer.parseInt(allChildren[i]));
+										System.out.println("Level " + depth + " Parent " + parent + ",Child "+ Integer.parseInt(allChildren[i]));
+									}
+								}
+							}
+							Cstmt.close();
+						} catch (SQLException e) {
+							System.out.println(e.getMessage());
+						}
+					}
+				}
+			}
+			Fstmt.close();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		if (!totalChildren.isEmpty())
+		{
+			int size = totalChildren.size();
+			children = new int [size];
+			for (int i=0;i<size;i++)
+				children[i] = (int)totalChildren.remove(0); // take first in queue
+		}
+		return children;	
 	}
 }
